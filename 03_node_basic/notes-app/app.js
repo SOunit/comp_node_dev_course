ea94ec9a9c15cc3d4d452c90a9b69bad9025beb9
SOunit <workbookwork7@gmail.com>
2021-08-28 08:23:53 -0700

    add npm package use sample

--- 03_node_basic/notes-app/app.js
+++ 03_node_basic/notes-app/app.js
@@ -1,3 +1,10 @@
+const validator = require('validator');
 const getNotes = require('./notes');
 
 console.log(getNotes());
+console.log(validator.isEmail('jack@test.com'));
+console.log(
+  validator.isURL(
+    'https://www.udemy.com/course/the-complete-nodejs-developer-course-2/learn/lecture/13728848#overview'
+  )
+);
