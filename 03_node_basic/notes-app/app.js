b92eb7231ee12b6cd04ef7ac73d19c59996c2d52
SOunit <workbookwork7@gmail.com>
2021-08-28 09:02:22 -0700

    add nodemon globaly and add command to run nodemon in dev

--- 03_node_basic/notes-app/app.js
+++ 03_node_basic/notes-app/app.js
@@ -2,10 +2,6 @@ const chalk = require('chalk');
 const getNotes = require('./notes');
 
 console.log(getNotes());
-console.log(chalk.green('Success!!!'));
-console.log(chalk.bold.green('bold green!'));
-console.log(chalk.inverse.green('inverse green!'));
-console.log(chalk.inverse.bold.green('inverse bold green!'));
 console.log(
   `npm ${chalk.inverse.yellow(
     'WARN'
